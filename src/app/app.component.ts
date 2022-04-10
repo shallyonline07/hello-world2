84622f0f5415b8652cceb1bef8f0b866faffb35f
CORP\asetiya <shallyonline07@gmail.com>
2022-04-10 12:52:15 -0400

    test done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hello-world2';
-// jkdhkjsdk
+  // jkdhkjsdk
+
+  ngOnInit(): void {
+    console.log('asdfsafd');
+
+  }
 
 }
